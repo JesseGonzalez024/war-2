c3a96952208828cc02cedb568dd8b9d118f51488
JesseGonzalez024 <jessegonzalez024@gmail.com>
2020-08-09 20:52:47 -0600

    notes added

---
+++ frontend/src/gameplay.js
@@ -0,0 +1,13 @@
+const newgame = document.getElementById("newgame")
+
+newgame.addEventListener("click", startgame)
+
+function startGame() {
+    // Upon game Initialization a randomized number of territories are to be distributed.
+    // Player object is to be created, and territories appeared.
+    renderMap()
+}
+
+function renderMap(){
+    // Display Territories and Continents
+}
