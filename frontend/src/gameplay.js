57c8afafdd12395098ea798e86d44ff245ea4402
JesseGonzalez024 <jessegonzalez024@gmail.com>
2020-08-21 09:23:25 -0600

    add notes for start of turn gameplay

--- frontend/src/gameplay.js
+++ frontend/src/gameplay.js
@@ -11,3 +11,18 @@ function startGame() {
 function renderMap(){
     // Display Territories and Continents
 }
+
+
+// Start of turn 
+//      - Identify all territories belonging to player and count them.
+//      - Based on total count of territories, player gets rewarded a certain number of troops.
+//      - Player can only select and place the troops wherever he already has troops.
+//      - "From Territories belonging to player, depemnding on the amount, give player available troops"
+//      - Player selects a territory and chooses how many available troops to deploy.
+//      - If the deployment is satisfcatofry the player can confirm the move each time he deploys troops.
+//      - This cycle is to continue untill there are no more remianing troops.
+//      - Once there are no more Troops left, the gameplay will switch to Attack mode.
+
+
+// Attack Mode
+//
